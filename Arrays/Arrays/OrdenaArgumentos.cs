39d7f226224f1ad71a6c36b491137e957068337f
Rafael Hiroshi Tagata <hiroshi_nuts@hotmail.com>
2016-09-29 20:32:43 -0300

    Fim capitulo 4 Arrays

---
+++ Arrays/Arrays/OrdenaArgumentos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class OrdenaArgumentos
+    {
+        static void Main(string[] args)
+        {
+            System.Array.Sort(args);
+
+            foreach(string arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+        }
+    }
+}
