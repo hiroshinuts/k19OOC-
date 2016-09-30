80f56e466b46388e001567a5203f20d6268e0733
Rafael Hiroshi Tagata <hiroshi_nuts@hotmail.com>
2016-09-29 21:11:44 -0300

    Fim cap 5 Static

---
+++ Static/Static/TestaContador.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static
+{
+    class TestaContador
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Contador: " + Conta.contador);
+
+            Conta c1 = new Conta();
+
+            Console.WriteLine("Numero da Primeira conta: " + c1.numero);
+            Console.WriteLine("Contador: " + Conta.contador);
+
+            Conta c2 = new Conta();
+
+            Console.WriteLine("Numero da Segunda conta: " + c2.numero);
+            Console.WriteLine("Contador: " + Conta.contador);
+
+            Conta.ZeraContador();
+        }
+    }
+}
