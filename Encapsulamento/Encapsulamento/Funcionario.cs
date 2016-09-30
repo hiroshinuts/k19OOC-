c9a979038cd7de6691230378afe2a637ca6cf3ff
Rafael Hiroshi Tagata <hiroshi_nuts@hotmail.com>
2016-09-29 22:20:18 -0300

    Fim capitulo 6 - encapsulamento

---
+++ Encapsulamento/Encapsulamento/Funcionario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulamento
+{
+    class Funcionario
+    {
+        private double salario;
+        private string nome;
+
+        public double Salario { get; set; }
+     
+        public string Nome { get; set; }
+   
+    }
+}
---
+++ Encapsulamento/Encapsulamento/Teste.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulamento
+{
+    class Teste
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f = new Funcionario();
+            f.Nome = "Rafael Hiroshi";
+            f.Salario = 2000;
+
+            Console.WriteLine(f.Nome);
+            Console.WriteLine(f.Salario);
+        }
+
+    }
+}
