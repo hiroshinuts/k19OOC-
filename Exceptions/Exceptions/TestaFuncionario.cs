b9c87c0bff2a4abdb9dad6edf10eccdd6371cef3
Rafael Hiroshi Tagata <hiroshi_nuts@hotmail.com>
2016-09-30 23:57:04 -0300

    FIM CAP 13 - EXCEPTIONS

---
+++ Exceptions/Exceptions/TestaFuncionario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    class TestaFuncionario
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f = new Funcionario();
+
+            try
+            {
+            f.AumentaSalario(-1000);
+            }
+            catch(ArgumentException e)
+            {
+                Console.WriteLine("Houve uma ArgumentException ao aumentar o salario");
+            }
+
+        }
+    }
+}
