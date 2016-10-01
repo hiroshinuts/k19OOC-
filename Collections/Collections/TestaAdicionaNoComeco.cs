72904ac324962b4df51c23fa7cbe931924dfe7d5
Rafael Hiroshi Tagata <hiroshi_nuts@hotmail.com>
2016-10-01 04:01:39 -0300

    COlletions , Listas - FIM CAP 15 e fim da apostila

---
+++ Collections/Collections/TestaAdicionaNoComeco.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class TestaAdicionaNoComeco
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+
+            long tempo = TestaAdicionaNoComeco.AdicionaNoComeco(arrayList);
+            Console.WriteLine("ArrayList: " + tempo + "ms");
+
+        }
+
+        public static long AdicionaNoComeco(IList lista)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            int size = 100000;
+
+            for (int i = 0; i < size; i++)
+            {
+                lista.Insert(0, 1);
+            }
+            sw.Stop();
+            return sw.ElapsedMilliseconds;
+        }
+
+    }
+}
---
+++ Collections/Collections/TestaAdicionaNoFinal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class TestaAdicionaNoFinal
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+            long tempo = TestaAdicionaNoFinal.AdicionaNoFinal(arrayList);
+            Console.WriteLine("ArrayList: " + tempo + "ms");
+        }
+        public static long AdicionaNoFinal(IList lista)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            int size = 100000;
+
+            for(int i = 0; i < size; i++)
+            {
+                lista.Add(i);
+            }
+            sw.Stop();
+            return sw.ElapsedMilliseconds;
+        }
+    }
+}
