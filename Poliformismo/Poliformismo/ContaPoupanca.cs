3521d93106bf899f4b8b281bf50e415c8b7243d3
Rafael Hiroshi Tagata <hiroshi_nuts@hotmail.com>
2016-09-30 21:01:35 -0300

    FIM CAP 8 - Poliformismo

---
+++ Poliformismo/Poliformismo/ContaPoupanca.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poliformismo
+{
+    class ContaPoupanca : Conta
+    {
+        public int DiaDoAniversario { get; set; }
+    }
+}
---
+++ Poliformismo/Poliformismo/GeradorDeExtrato.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poliformismo
+{
+    class GeradorDeExtrato
+    {
+        public void ImprimeExtratoBasico(Conta c)
+        {
+            DateTime agora = DateTime.Now;
+            string horario = String.Format("{0:d/M/yyyy HH:mm:ss}", agora);
+
+            Console.WriteLine("DATA: " + horario);
+            Console.WriteLine("SALDO: " + c.Saldo);
+        }
+    }
+}
