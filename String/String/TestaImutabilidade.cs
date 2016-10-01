81417a3a9779635213edefb8ed214ec22c421b28
Rafael Hiroshi Tagata <hiroshi_nuts@hotmail.com>
2016-10-01 03:20:44 -0300

    CAPITULO 14 - STRING

---
+++ String/String/TestaImutabilidade.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String
+{
+    class TestaImutabilidade
+    {
+        static void Main(string[] args)
+        {
+            string nome = "Rafael Hiroshi";
+            string nomeAlterado = nome.ToUpper();
+
+            Console.WriteLine(nome);
+            Console.WriteLine(nomeAlterado);
+        }
+    }
+}
